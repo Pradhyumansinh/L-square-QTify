b4d150d7f58467327db586f95883f8d3e2ad7958
pradhyumansinh <jadejapradhyumansinh545@gmail.com>
2024-05-13 22:39:54 +0530

    use Hook before each component

--- qtify/src/components/Navbar/Navbar.js
+++ qtify/src/components/Navbar/Navbar.js
@@ -4,8 +4,11 @@ import { Box } from "@mui/material";
 import "./Navbar.css";
 import SearchBar from "../SearchBar/SearchBar";
 import Button from "../Button/Button";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [count, setCount] = useState(0);
+
     return (
         <Box className="navbar">
             <Logo />
